266f98be8614de45bdcc2f1991ca82bad2e2e5f3
ravi64p <sachinkumar.332006@gmail.com>
2025-09-07 00:27:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,86 @@
+export const data = [
+    {
+        question: "Which device is required for the internet connection",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which continent has the highest number of countries",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question: "Junk e-mail is also called?",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archieved",
+        option4: "Bin",
+        ans: 1,
+    },
+    {
+        question: "A computer cannot BOOT if it does not have the?",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+        ans: 3,
+    },
+    {
+        question: "First page of website is termed as?",
+        option1: "Index Page",
+        option2: "Homepage",
+        option3: "Sitemap",
+        option4: "Pen Drive",
+        ans: 2,
+    },
+    {
+    question: "What is the capital of India?",
+    option1: "Kolkata",
+    option2: "New Delhi",
+    option3: "Ranchi",
+    option4: "Mumbai",
+    ans: 2
+    },
+    {
+    question: "Which planet is known as the Red Planet?",
+    option1: "Earth",
+    option2: "Venus",
+    option3: "Mars",
+    option4: "Jupiter",
+    ans: 3
+
+    },
+    {
+    question: "Which is the largest ocean in the world?",
+    option1: "Atlantic Ocean",
+    option2: "Indian Ocean",
+    option3: "Pacific Ocean",
+    option4: "Arctic Ocean",
+    ans: 3
+
+    },
+    {
+    question: "Who is known as the Father of the Nation in India?",
+    option1: "Bhagat Singh",
+    option2: "Mahatma Gandhi",
+    option3: "Jawaharlal Nehru",
+    option4: "Subhas Chandra Bose",
+    ans: 2
+
+    },
+    {
+    question: "Which gas do plants absorb during photosynthesis?",
+    option1: "Oxygen",
+    option2: "Carbon Dioxide",
+    option3: "Nitrogen",
+    option4: "Hydrogen",
+    ans: 2
+
+    },
+];
